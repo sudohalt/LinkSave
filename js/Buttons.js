f708d17d22094f60c60c79baaa956bc071abc1ff
Umayah Abdennabi <abden003@gmail.com>
2015-05-26 00:51:56 -0500

    Updated git with KingMak's changes

---
+++ js/Buttons.js
@@ -0,0 +1,47 @@
+function saveTabs()
+{
+    // current tab = var queryInfo = { active: true, currentWindow: true };
+    // all other tabs = var queryInfo = { active: false, currentWindow: true };
+
+    var queryInfo = {};
+    chrome.tabs.query(queryInfo, function(tabs)
+    {   
+        var tabCount = 1;
+        var tabUrls = "";
+        var tabTitles = "";
+        var currentTabUrl = "";
+        
+        for (i = 0; i < tabs.length; i++)
+        {
+            currentTabUrl = tabs[i].url;
+            if (currentTabUrl.substring(0, 9) != "chrome://" && 
+                currentTabUrl.substring(0, 12) != "view-source:" &&
+                currentTabUrl.substring(0, 19) != "chrome-extension://")
+            {
+            	tabUrls += tabs[i]url + "\n";
+                tabTitles += tabs[i].title + "\n";
+                tabCount++;  
+            }
+        }
+    });
+}
+
+function SaveLinks()
+{
+	saveTabs();
+	popupStatus("Links Saved");
+}
+
+function OpenLinks()
+{
+	popupStatus("Links Opened");
+}
+
+function popupStatus(statusText)
+{
+    document.getElementById('StatusLabel').textContent = statusText;
+}
+
+// FUNCTION CHAINGING is messed up
+document.getElementById("SaveLinks").addEventListener("click", function(){ SaveLinks() });
+document.getElementById("OpenLinks").addEventListener("click", function(){ OpenLinks() });
---
+++ js/StatusLabel.js
@@ -0,0 +1,12 @@
+// very creative hack?
+var len = document.getElementById("StatusLabel").innerHTML;
+if (len > 0)
+{
+	var canvas = document.createElement('canvas');
+	var ctx = canvas.getContext("2d"); 
+	ctx.font = "17px Arial";        
+	var styleWidth = ctx.measureText(len).width;
+	var styleWidthString = styleWidth.toString() + "px";
+
+	document.getElementById("StatusLabel").style.width = styleWidthString;
+}
