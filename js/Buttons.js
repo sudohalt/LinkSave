15c3fe19f7393c88d417ec40c0d6cae69098c413
Umayah Abdennabi <abden003@gmail.com>
2015-05-28 01:14:18 -0500

    Changed the button layout, made the checkboxes go away when you click the button to save the links, also made only the links of the currently opened window appear, also added the functionarlity of saving bookmarks

--- js/Buttons.js
+++ js/Buttons.js
@@ -1,15 +1,24 @@
+var showStatsBool = false;
+
 function saveTabs()
 {
     // current tab = var queryInfo = { active: true, currentWindow: true };
     // all other tabs = var queryInfo = { active: false, currentWindow: true };
 
-    var queryInfo = {};
+    var queryInfo = {'lastFocusedWindow': true};
+    var bookmarkId = null;
+    chrome.bookmarks.create({'title': 'LinkSave bookmarks'},
+                          function(newFolder) {
+    console.log("added folder: " + newFolder.title);
+    bookmarkId = newFolder.id;
+    });
     chrome.tabs.query(queryInfo, function(tabs)
     {   
         var tabCount = 1;
         var tabUrls = "";
         var tabTitles = "";
         var currentTabUrl = "";
+        // Creat folder
         document.getElementById("openLinks").innerHTML = "";
         for (i = 0; i < tabs.length; i++)
         {
@@ -18,11 +27,21 @@ function saveTabs()
                 currentTabUrl.substring(0, 12) != "view-source:" &&
                 currentTabUrl.substring(0, 19) != "chrome-extension://")
             {
-            	tabUrls += tabs[i].url + "\n";
-                tabTitles += tabs[i].title + "\n";
-                tabCount++;  
+            	chrome.bookmarks.create({'parentId': bookmarkId,
+                               'title': tabs[i].title,
+                               'url': tabs[i].url});
             }
             document.getElementById("openLinks").innerHTML += "<li>" + tabs[i].title + "</li>";
+        }
+    });
+}
+
+function viewTabs() {
+    var queryInfo = {lastFocusedWindow: true};
+    chrome.tabs.query(queryInfo, function(tabs) {
+        document.getElementById("openLinks").innerHTML = "";
+        for (i = 0; i < tabs.length; i++) {
+            document.getElementById("openLinks").innerHTML += "<li><input type=\"checkbox\" checked>" + tabs[i].title + "</li>";
         }
     });
 }
@@ -30,6 +49,7 @@ function saveTabs()
 function SaveLinks()
 {
 	saveTabs();
+    document.getElementById("SaveLinks").hidden = true;
 	popupStatus("Links Saved!");
 }
 
@@ -43,7 +63,19 @@ function popupStatus(statusText)
     document.getElementById('StatusLabel').textContent = statusText;
 }
 
-saveTabs();
+function ShowStats() {
+    if (showStatsBool != true) {
+        var totalBlocked = 123; 
+        var shareString = "<br>Share on:<br><img src=''><img src=''><img src=''>"
+        document.getElementById("StatsBox").innerHTML = "<b>Total: </b>" + totalBlocked + shareString; 
+        showStatsBool = true;
+    } else {
+        document.getElementById("StatsBox").innerHTML = "";
+        showStatsBool = false;
+    }
+}
+
+viewTabs();
 // FUNCTION CHAINGING is messed up
-document.getElementById("SaveLinks").addEventListener("click", function(){ SaveLinks() });
-document.getElementById("OpenLinks").addEventListener("click", function(){ OpenLinks() });
+document.getElementById("SaveLinks").addEventListener("click", function() { SaveLinks() });
+document.getElementById("Stats").addEventListener("click", function() { ShowStats() });
