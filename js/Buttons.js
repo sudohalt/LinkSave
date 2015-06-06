04f69939e6ee1259a4c109d4bf58200fc0f51180
Umayah Abdennabi <abden003@umn.edu>
2015-06-05 23:21:20 -0500

    Added the info label, and added a text box for the new folder
    The info link needs to link to LinkSave site.  The new folder text box
    will be enabled when the user clicks the checkbox to create in a new
    folder

--- js/Buttons.js
+++ js/Buttons.js
@@ -1,6 +1,6 @@
 /* All rights reserved LinkSave
  *
- * Authors: Muhammad Khalid and Umayah Abdennabi
+ * Authors: Muhammad Khalid (KingMak) and Umayah Abdennabi (sudohalt)
  *
  * This code is open source and is lincensed under the <insert licnese here>
  * Source code is available on GitHub: <link to github>
@@ -9,7 +9,8 @@
 
 function search_for_title(bookmarks, title) {
   for(var i=0; i < bookmarks.length; i++) { 
-    if(bookmarks[i].url != null && bookmarks[i].title == title) {
+    if(bookmarks[i].url == null && bookmarks[i].title == title) {
+      console.log("Found bookmark folder")
       return bookmarks[i].id;
     } else {
       if(bookmarks[i].children) {  
@@ -33,23 +34,81 @@ function saveTabs() {
      * everything in the callback functions. */
     chrome.bookmarks.getTree(
       function(bookmarks) {
-        var bookmarkId = search_for_url(bookmarks, "LinkSave");
+        var bookmarkId = search_for_title(bookmarks, "LinkSave bookmarks");
         if (bookmarkId == false) {
-
+          console.log("Creating new bookmark folder");
+          chrome.bookmarks.create({'title': 'LinkSave bookmarks'},
+              function(newFolder) {
+                  var bookmarkId = newFolder.id;
+                  chrome.tabs.query(queryInfo, 
+                      function(tabs) {   
+                        // Clear the scroll box, and update it with the links that were saved
+                        // Go through all the open tabs and save each one, and update the scroll view
+                        var newScrollView = ""
+                        if (document.getElementById("saveInNew").checked) {
+                          var folderTitle = ""
+                          if (document.getElementById("saveInNewName").value == "") {
+                            var date = new Date()
+                            folderTitle = date.getHours() + ":" + date.getMinutes() + " " + (date.getMonth() + 1) + "/" + date.getDay() + "/" + date.getFullYear();
+                          } else {
+                            folderTitle = document.getElementById("saveInNewName").value;
+                          }
+                          var tempScrollView = document.getElementById('openLinks').innerHTML;
+                          chrome.bookmarks.create({'parentId' : bookmarkId, 'title' : folderTitle},
+                            function (subNewFolder) {
+                              document.getElementById('openLinks').innerHTML = tempScrollView;
+                              for (i = 0; i < tabs.length; i++) {
+                                if (document.getElementById(i.toString()).checked) {
+                                  chrome.bookmarks.create({'parentId': subNewFolder.id,
+                                    'title': tabs[i].title,
+                                    'url': tabs[i].url},
+                                    function(obj) {
+                                      if (chrome.runtime.lastError) {
+                                        console.log("Error while accessing id. Message: " + chrome.runtime.lastError.message);
+                                      }
+                                  });
+                                  newScrollView += "<li>" + tabs[i].title + "</li>";
+                                }
+                              }  
+                          });
+                        } else {
+                            for (i = 0; i < tabs.length; i++) {
+                              if (document.getElementById(i.toString()).checked) {
+                                chrome.bookmarks.create({'parentId': bookmarkId,
+                                  'title': tabs[i].title,
+                                  'url': tabs[i].url},
+                                  function(obj) {
+                                    if (chrome.runtime.lastError) {
+                                      console.log("Error while accessing id. Message: " + chrome.runtime.lastError.message);
+                                    }
+                                });
+                                newScrollView += "<li>" + tabs[i].title + "</li>";
+                              }
+                            }
+                        }
+                        document.getElementById("openLinks").innerHTML = newScrollView;
+                  });
+              });
         } else {
           chrome.tabs.query(queryInfo, 
             function(tabs) {   
               // Clear the scroll box, and update it with the links that were saved
               // Go through all the open tabs and save each one, and update the scroll view
               var newScrollView = ""
-              var recreate = false;
-              if (document.getElementById("newFolder").checked) {
-                chrome.bookmarks.create({'parentId' : bookmarkId, 'title' : getCurrentDate(),
-                  function (newFolder) {
+              if (document.getElementById("saveInNew").checked) {
+                var folderTitle = ""
+                if (document.getElementById("saveInNewName").value == "") {
+                  var date = new Date()
+                  folderTitle = date.getHours() + ":" + date.getMinutes() + " " + (date.getMonth() + 1) + "/" + date.getDay() + "/" + date.getFullYear();
+                } else {
+                  folderTitle = document.getElementById("saveInNewName").value;
+                }                var tempScrollView = document.getElementById('openLinks').innerHTML;
+                chrome.bookmarks.create({'parentId' : bookmarkId, 'title' : folderTitle},
+                  function (subNewFolder) {
+                    document.getElementById('openLinks').innerHTML = tempScrollView;
                     for (i = 0; i < tabs.length; i++) {
                       if (document.getElementById(i.toString()).checked) {
-                        console.log("Bookmark ID two" + newFolder.id);
-                        chrome.bookmarks.create({'parentId': newFolder.id,
+                        chrome.bookmarks.create({'parentId': subNewFolder.id,
                           'title': tabs[i].title,
                           'url': tabs[i].url},
                           function(obj) {
@@ -60,12 +119,10 @@ function saveTabs() {
                         newScrollView += "<li>" + tabs[i].title + "</li>";
                       }
                     }  
-                  }
                 });
               } else {
                 for (i = 0; i < tabs.length; i++) {
                   if (document.getElementById(i.toString()).checked) {
-                    console.log("Bookmark ID two" + bookmarkId);
                     chrome.bookmarks.create({'parentId': bookmarkId,
                       'title': tabs[i].title,
                       'url': tabs[i].url},
@@ -82,67 +139,10 @@ function saveTabs() {
             });
         }
     });
-
-                default:
-                    chrome.bookmarks.create({'title': 'LinkSave bookmarks'},
-                        function(newFolder) {
-                            var bookmarkId = newFolder.id;
-                            chrome.tabs.query(queryInfo, 
-                                function(tabs) {   
-                                    // Clear the scroll box, and update it with the links that were saved
-                                    // Go through all the open tabs and save each one, and update the scroll view
-                                    var newScrollView = ""
-                                    if (document.getElementById("newFolder").checked) {
-                                        chrome.bookmarks.create({'parentId' : bookmarkId, 'title' : getCurrentDate(),
-                                            function (newFolder) {
-                                                for (i = 0; i < tabs.length; i++) {
-                                                    if (document.getElementById(i.toString()).checked) {
-                                                        console.log("Bookmark ID two" + newFolder.id);
-                                                        chrome.bookmarks.create({'parentId': newFolder.id,
-                                                            'title': tabs[i].title,
-                                                            'url': tabs[i].url},
-                                                            function(obj) {
-                                                                if (chrome.runtime.lastError) {
-                                                                    console.log("Error while accessing id. Message: " + chrome.runtime.lastError.message);
-                                                                    console.log("This probably means that you deleted the folder, no worry I will recreate it");
-                                                                    recreate = true;
-                                                                    break;
-                                                                }
-                                                            });
-                                                        newScrollView += "<li>" + tabs[i].title + "</li>";
-                                                    }
-                                                }  
-                                            }
-                                        });
-                                    } else {
-                                        for (i = 0; i < tabs.length; i++) {
-                                            if (document.getElementById(i.toString()).checked) {
-                                                console.log("Bookmark ID two" + bookmarkId);
-                                                chrome.bookmarks.create({'parentId': bookmarkId,
-                                                    'title': tabs[i].title,
-                                                    'url': tabs[i].url},
-                                                    function(obj) {
-                                                        if (chrome.runtime.lastError) {
-                                                            console.log("Error while accessing id. Message: " + chrome.runtime.lastError.message);
-                                                            console.log("This probably means that you deleted the folder, no worry I will recreate it");
-                                                            recreate = true;
-                                                            break;
-                                                        }
-                                                    });
-                                                newScrollView += "<li>" + tabs[i].title + "</li>";
-                                            }
-                                        }
-                                    }
-                                    document.getElementById("openLinks").innerHTML = newScrollView;
-                            });
-                        });
-                    break;
-
-            }
-        });
     // Hide the Save Links button (will reappear when extension is reopened) and show status label
+    document.getElementById('saveInNewP').hidden = true;
     document.getElementById("SaveLinks").hidden = true;
-    document.getElementById("StatusLabel").textContent = "Links Saved!";
+    document.getElementById("StatusLabel").textContent = "Links Saved in bookmarks!";
     document.getElementById("TopLabel").textContent = "Saved Tabs"
 }
 
@@ -166,4 +166,15 @@ document.getElementById("SaveLinks").addEventListener("click",
     function() { 
         saveTabs() 
     }
+);
+document.getElementById("saveInNew").addEventListener("click",
+  function() {
+    var cond = !document.getElementById('saveInNew').checked;
+    if (cond) {
+      document.getElementById('saveInNewName').value = "";
+      document.getElementById('saveInNewName').disabled = true;
+    } else {
+      document.getElementById('saveInNewName').disabled = false;
+    }
+  }
 );
