891e8dd1e0bf6e142dd4a60a3e0a5aa25c8a4d13
Umayah Abdennabi <abden003@gmail.com>
2015-06-05 19:46:29 -0500

    Updating the file to not use chrome.storage

--- js/Buttons.js
+++ js/Buttons.js
@@ -31,156 +31,61 @@ function saveTabs() {
      * exist use it by getting its id from chrome.storage.sync (that is where 
      * everything is stored).  Since the operations are asynchronous we must do
      * everything in the callback functions. */
-    chrome.storage.sync.get('linkSaveFolderExists',
-        function(exists) {
-            if (chrome.runtime.lastError) {
-                console.log("Error unable to determine whether LinkSave folder exists. Message: " + chrome.runtime.lastError.message);
-                return;
-            }
-            /* The first case means that the LinkSave folder exists, and the default
-             * case is when the LinkSave folder does not exist.  If it does exists
-             * we access it and create the bookmarks within the folder, and if it
-             * doesn't we create the bookmark folder and then save the bookmarks.
-             * Since the chrome api is asynchronous we have nested asynchrounous 
-             * calls to make sure everything is done in the right order.  A problem
-             * arises when the user manually deletes the LinkSave folder in this
-             * case we have to recreate it and this is handled in case 1. */
-            switch(exists["linkSaveFolderExists"]) {
-                case 1:
-                    /* We have chosen to use chrome.storage.sync instead of 
-                     * chrome.storage.sync because we do not want to recreate
-                     * the bookmark folder when the user uses a new device */
-                    chrome.storage.sync.get('linkSaveFolderId',
-                        function(folderId) {
+    chrome.bookmarks.getTree(
+      function(bookmarks) {
+        var bookmarkId = search_for_url(bookmarks, "LinkSave");
+        if (bookmarkId == false) {
+
+        } else {
+          chrome.tabs.query(queryInfo, 
+            function(tabs) {   
+              // Clear the scroll box, and update it with the links that were saved
+              // Go through all the open tabs and save each one, and update the scroll view
+              var newScrollView = ""
+              var recreate = false;
+              if (document.getElementById("newFolder").checked) {
+                chrome.bookmarks.create({'parentId' : bookmarkId, 'title' : getCurrentDate(),
+                  function (newFolder) {
+                    for (i = 0; i < tabs.length; i++) {
+                      if (document.getElementById(i.toString()).checked) {
+                        console.log("Bookmark ID two" + newFolder.id);
+                        chrome.bookmarks.create({'parentId': newFolder.id,
+                          'title': tabs[i].title,
+                          'url': tabs[i].url},
+                          function(obj) {
                             if (chrome.runtime.lastError) {
-                                console.log("Error: Unable to get LinkSave folder id. Message: " + chrome.runtime.lastError.message);
-                                return;
+                              console.log("Error while accessing id. Message: " + chrome.runtime.lastError.message);
                             }
-                            var bookmarkId = folderId["linkSaveFolderId"];
-                            console.log("Bookmare ID one" + bookmarkId)
-                            chrome.tabs.query(queryInfo, 
-                                function(tabs) {   
-                                    // Clear the scroll box, and update it with the links that were saved
-                                    // Go through all the open tabs and save each one, and update the scroll view
-                                    var newScrollView = ""
-                                    var recreate = false;
-                                    if (document.getElementById("newFolder").checked) {
-                                        chrome.bookmarks.create({'parentId' : bookmarkId, 'title' : getCurrentDate(),
-                                            function (newFolder) {
-                                                for (i = 0; i < tabs.length; i++) {
-                                                    if (document.getElementById(i.toString()).checked) {
-                                                        console.log("Bookmark ID two" + newFolder.id);
-                                                        chrome.bookmarks.create({'parentId': newFolder.id,
-                                                            'title': tabs[i].title,
-                                                            'url': tabs[i].url},
-                                                            function(obj) {
-                                                                if (chrome.runtime.lastError) {
-                                                                    console.log("Error while accessing id. Message: " + chrome.runtime.lastError.message);
-                                                                    console.log("This probably means that you deleted the folder, no worry I will recreate it");
-                                                                    recreate = true;
-                                                                    break;
-                                                                }
-                                                            });
-                                                        newScrollView += "<li>" + tabs[i].title + "</li>";
-                                                    }
-                                                }  
-                                            }
-                                        });
-                                    } else {
-                                        for (i = 0; i < tabs.length; i++) {
-                                            if (document.getElementById(i.toString()).checked) {
-                                                console.log("Bookmark ID two" + bookmarkId);
-                                                chrome.bookmarks.create({'parentId': bookmarkId,
-                                                    'title': tabs[i].title,
-                                                    'url': tabs[i].url},
-                                                    function(obj) {
-                                                        if (chrome.runtime.lastError) {
-                                                            console.log("Error while accessing id. Message: " + chrome.runtime.lastError.message);
-                                                            console.log("This probably means that you deleted the folder, no worry I will recreate it");
-                                                            recreate = true;
-                                                            break;
-                                                        }
-                                                    });
-                                                newScrollView += "<li>" + tabs[i].title + "</li>";
-                                            }
-                                        }
-                                    }
-                                    // If the user manually deletes the LinkSave 
-                                    // folder we have to recreate it.
-                                    if (recreate) {
-                                        chrome.bookmarks.create({'title': 'LinkSave bookmarks'},
-                                            function(newFolder) {
-                                                console.log("added folder: " + newFolder.title);
-                                                chrome.storage.sync.set({'linkSaveFolderExists' : 1},
-                                                    function() {
-                                                        console.log("Created new storage");
-                                                    });
-                                                chrome.storage.sync.set({'linkSaveFolderId' : newFolder.id});
-                                                var bookmarkId = newFolder.id;
-                                                chrome.tabs.query(queryInfo, 
-                                                    function(tabs) {   
-                                                        // Clear the scroll box, and update it with the links that were saved
-                                                        // Go through all the open tabs and save each one, and update the scroll view
-                                                        var newScrollView = ""
-                                                        if (document.getElementById("newFolder").checked) {
-                                                            chrome.bookmarks.create({'parentId' : bookmarkId, 'title' : getCurrentDate(),
-                                                                function (newFolder) {
-                                                                    for (i = 0; i < tabs.length; i++) {
-                                                                        if (document.getElementById(i.toString()).checked) {
-                                                                            console.log("Bookmark ID two" + newFolder.id);
-                                                                            chrome.bookmarks.create({'parentId': newFolder.id,
-                                                                                'title': tabs[i].title,
-                                                                                'url': tabs[i].url},
-                                                                                function(obj) {
-                                                                                    if (chrome.runtime.lastError) {
-                                                                                        console.log("Error while accessing id. Message: " + chrome.runtime.lastError.message);
-                                                                                        console.log("This probably means that you deleted the folder, no worry I will recreate it");
-                                                                                        recreate = true;
-                                                                                        break;
-                                                                                    }
-                                                                                });
-                                                                            newScrollView += "<li>" + tabs[i].title + "</li>";
-                                                                        }
-                                                                    }  
-                                                                }
-                                                            });
-                                                        } else {
-                                                            for (i = 0; i < tabs.length; i++) {
-                                                                if (document.getElementById(i.toString()).checked) {
-                                                                    console.log("Bookmark ID two" + bookmarkId);
-                                                                    chrome.bookmarks.create({'parentId': bookmarkId,
-                                                                        'title': tabs[i].title,
-                                                                        'url': tabs[i].url},
-                                                                        function(obj) {
-                                                                            if (chrome.runtime.lastError) {
-                                                                                console.log("Error while accessing id. Message: " + chrome.runtime.lastError.message);
-                                                                                console.log("This probably means that you deleted the folder, no worry I will recreate it");
-                                                                                recreate = true;
-                                                                                break;
-                                                                            }
-                                                                        });
-                                                                    newScrollView += "<li>" + tabs[i].title + "</li>";
-                                                                }
-                                                            }
-                                                        }
-                                                        document.getElementById("openLinks").innerHTML = newScrollView;
-                                                });
-                                        });
-                                    } else {
-                                        document.getElementById("openLinks").innerHTML = newScrollView;
-                                    }
-                                });
                         });
-                    break;
+                        newScrollView += "<li>" + tabs[i].title + "</li>";
+                      }
+                    }  
+                  }
+                });
+              } else {
+                for (i = 0; i < tabs.length; i++) {
+                  if (document.getElementById(i.toString()).checked) {
+                    console.log("Bookmark ID two" + bookmarkId);
+                    chrome.bookmarks.create({'parentId': bookmarkId,
+                      'title': tabs[i].title,
+                      'url': tabs[i].url},
+                      function(obj) {
+                      if (chrome.runtime.lastError) {
+                        console.log("Error while accessing id. Message: " + chrome.runtime.lastError.message);
+                      }
+                    });
+                    newScrollView += "<li>" + tabs[i].title + "</li>";
+                  }
+                }
+              }
+              document.getElementById("openLinks").innerHTML = newScrollView;
+            });
+        }
+    });
+
                 default:
                     chrome.bookmarks.create({'title': 'LinkSave bookmarks'},
                         function(newFolder) {
-                            console.log("added folder: " + newFolder.title);
-                            chrome.storage.sync.set({'linkSaveFolderExists' : 1},
-                                function() {
-                                    console.log("Created new storage");
-                                });
-                            chrome.storage.sync.set({'linkSaveFolderId' : newFolder.id});
                             var bookmarkId = newFolder.id;
                             chrome.tabs.query(queryInfo, 
                                 function(tabs) {   
